38bb4ab6d5112d17a1146025046e4bc08ad856c1
Initial Commit <newhite7070@gmail.com>
2020-07-08 15:53:48 -0400

    Done

---
+++ CodingEventsDemo/Controllers/EventCagtegoryController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CodingEventsDemo.Data;
+using CodingEventsDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodingEventsDemo.Controllers
+{
+    public class EventCategoryController : Controller
+    {
+        private EventDbContext context;
+
+        public EventCategoryController(EventDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
+        // GET: /<controller>/
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.title = "All Categories";
+            List<EventCategory> categories = context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
